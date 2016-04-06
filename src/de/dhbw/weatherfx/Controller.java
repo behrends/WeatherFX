4ea1695e19585ec5eee6ecd0d4679a27a05aca32
Erik Behrends <erik.behrends@gmail.com>
2016-04-06 13:45:21 +0200

    Change label text on button click

--- src/de/dhbw/weatherfx/Controller.java
+++ src/de/dhbw/weatherfx/Controller.java
@@ -1,4 +1,18 @@
 package de.dhbw.weatherfx;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class Controller {
+    @FXML
+    Button myButton;
+
+    @FXML
+    Label myLabel;
+
+    public void myButtonClicked(ActionEvent actionEvent) {
+        myLabel.setText("wurde geklickt");
+    }
 }
