5c16ec66818be7019dc92e61e8f67e69dc67fe64
Erik Behrends <erik.behrends@gmail.com>
2016-04-08 08:59:30 +0200

    Button id nicht nötig

--- src/de/dhbw/weatherfx/Controller.java
+++ src/de/dhbw/weatherfx/Controller.java
@@ -2,13 +2,9 @@ package de.dhbw.weatherfx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class Controller {
-    @FXML
-    Button myButton;
-
     @FXML
     Label myLabel;
 
